fecc9902649afb45d673b5edb369ae89b00eac17
armaandesai <armaandesayuno@gmail.com>
2023-04-16 03:01:05 -0500

    WIP: 4748482 Added recording audio and converting to base64

--- src/CollisionCenter.jsx
+++ src/CollisionCenter.jsx
@@ -1,9 +1,11 @@
+import "./collisionCenter.css"
+
 function CollisionCenter() {
   return (
     <div className="mainInterface">
       <h1>Collision Center</h1>
       <p>Here is a list of collision centers in the area:</p>
-      <ul>
+      <ul className="collisionCenter">
         <li>Collision Center 1</li>
         <li>Collision Center 2</li>
         <li>Collision Center 3</li>
